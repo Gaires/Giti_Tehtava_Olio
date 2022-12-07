53b042911218cd290362131acaf7a59c7262f606
Gaires <115219293+Gaires@users.noreply.github.com>
2022-12-07 11:51:59 +0200

    Tehtavan osio 1

--- src/Lelu_Tehtava.java
+++ src/Lelu_Tehtava.java
@@ -2,8 +2,25 @@
 public class Lelu_Tehtava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Lelu Lelu1 = new Lelu();
+		Lelu1.Print();
 	}
 
-}
+}  //End of Main
+
+	class Lelu{
+		
+		String Name = "Fort";
+		int Weight = 10;
+		
+		
+		public void Print() {
+			
+			System.out.println("Name of the toy is " + Name + " and the toy weights " + Weight+"kg");
+			
+		}
+		}
+	
+	
+	
