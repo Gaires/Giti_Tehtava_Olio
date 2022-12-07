2d4926ecb02d4620dcdcb77df10ca88cfcda6d70
Gaires <115219293+Gaires@users.noreply.github.com>
2022-12-07 13:08:43 +0200

    Huomenna? Jatkuu!

--- src/Lelu_Tehtava.java
+++ src/Lelu_Tehtava.java
@@ -3,24 +3,61 @@ public class Lelu_Tehtava {
 
 	public static void main(String[] args) {
 		
-		Lelu Lelu1 = new Lelu();
-		Lelu1.Print();
+		//Lelu Lelu1 = new Lelu();
+		Lelu VieteriLelu = new Lelu();
+		//Lelu1.Print();
+		VieteriLelu.Print();
+		
 	}
 
 }  //End of Main
 
 	class Lelu{
 		
-		String Name = "Fort";
-		int Weight = 10;
+		String Nimi = "Automobiili";
+		int Paino = 1;
 		
 		
+		protected void Print() {
+			
+			System.out.println("Lelun nimi on " + Nimi + " ja se painaa " + Paino + "kg");
+			
+			
+		}
+		} //Lelu ends
+	
+	class VieteriLelu extends Lelu {
+		
+	
+		private boolean Viritetty;
+		private int Nopeus;
+		private int Kiihdyta;
+		
+		public VieteriLelu() {
+		
+			String Nimi = "Automobiili";
+			int Paino = 1;
+			Viritetty = true;
+			Nopeus = 0;
+			Kiihdyta = 0;
+			
+		}
+		
+		public void Kiihdyta(int Kiihdyta) {
+			Kiihdyta = Nopeus +1;
+			
+		}
+		
+		public void Nopeus(int Nopeus) {
+			
+			System.out.println("Auton vauhti " + Nopeus);
+		}
 		public void Print() {
 			
-			System.out.println("Name of the toy is " + Name + " and the toy weights " + Weight+"kg");
-			
+			System.out.println(Nopeus);
+			System.out.println("VITTU");
 		}
-		}
-	
-	
+
+		
+	} //VieteriLelu ends
 	
